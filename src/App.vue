c16911af03cd62d76de577d20f56fc86d4796724
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-02-21 14:19:22 -0300

    feat: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" id="app">
+    <Navbar :title="title" @getQuery="returnQuery($event)" />
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(poke, i) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :num="i + 1" :url="poke.url" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+import Navbar from "./components/Navbar.vue";
+import Pokemon from "./components/Pokemon.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Pokemon,
+  },
+  data() {
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      title: process.env.VUE_APP_TITLE,
+    }
+  },
+  computed: {
+    
+  },
+  methods: {
+    returnQuery($event) {
+      this.filteredPokemons = this.pokemons;
+
+      if ($event === undefined || $event === '' || $event === ' ') { 
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name === $event);
+      }
+    }
+  },
+  created: function() {
+    axios.get(process.env.VUE_APP_API_URL)
+      .then(res => {
+        this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
+      })
+      .catch(error => console.error(error));    
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
